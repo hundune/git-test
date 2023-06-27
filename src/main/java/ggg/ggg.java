11878b564146904d5fc420fe377f872b0e298d13
hundune <108261046+hundune@users.noreply.github.com>
2023-06-27 19:09:00 +0800

    Update ggg.java

--- src/main/java/ggg/ggg.java
+++ src/main/java/ggg/ggg.java
@@ -8,5 +8,8 @@ public class ggg {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello2");
+        System.out.println("hello2");
+        System.out.println("hello2");
     }
 }
